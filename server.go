3953f6e10d5ce37f8f030322ca682fd09b505055
esm <elliot.metsger@persefoni.com>
2022-01-25 09:45:22 -0500

    Add command line flags for specifying: - server root (the base directory on the server where content is served from) - verbose flag - scale group (a key that groups instances of the server scaled by docker-compose up --scale....) - web root (HTTP request path prefix that should be stripped from the request prior to resolving the content to be served)
    Added more information to the template model.

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"html/template"
 	"log"
@@ -20,19 +20,32 @@ const (
 	contentRoot = "/www"
 )
 
+var isVerbose bool
+
 func main() {
 
+	serverRoot := flag.String("serverRoot", contentRoot, "server root dir")
+	webRoot := flag.String("webRoot", "", "content of this server is subordinate to this HTTP request path prefix")
+	scaleGroup := flag.String("group", "not specified", "a key that groups services that are scaled together")
+	isDebug := flag.Bool("debug", false, "enable debugging")
+	flag.Parse()
+
+	isVerbose = *isDebug
+
 	model := struct {
-		Hostname string
-		Request  string
+		Hostname   string
+		Request    string
+		ScaleGroup string
+		Date       string
 	}{
-		Hostname: mustHostname(),
+		Hostname:   mustHostname(),
+		ScaleGroup: *scaleGroup,
 	}
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		var err error
 
-		filePath := path.Join(contentRoot, r.URL.Path)
+		filePath := path.Join(*serverRoot, stripRoot(*webRoot, r.URL.Path))
 		log.Printf("Serving '%s'", filePath)
 
 		if strings.HasSuffix(filePath, ".tmpl") {
@@ -44,6 +57,7 @@ func main() {
 		if tmplPath, exists := isTemplateRequest(filePath); exists {
 			dump, _ := httputil.DumpRequest(r, false)
 			model.Request = string(dump)
+			model.Date = time.Now().Format(time.RFC3339)
 			if filePath, err = processTemplate(tmplPath, model); err != nil {
 				writeErr(w, err)
 			}
@@ -75,15 +89,18 @@ func main() {
 	http.HandleFunc("/", handler)
 
 	listenPort := mustGetIntOrDefault(envMsPort, defaultPort)
-	log.Printf("Listening on port %d, serving from directory '%s'", listenPort, contentRoot)
+	log.Printf("Listening on port %d, serving from directory '%s'", listenPort, *serverRoot)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil))
 }
 
+// responds to the request with a 500 and the content of the error
 func writeErr(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Write([]byte(err.Error()))
 }
 
+// mustGetIntorDefault answers the value of the requested env var as an integer, or the default value if none exists.
+// If the value of the requested env var cannot be converted to an int, mustGetIntOrDefault panics.
 func mustGetIntOrDefault(env string, defaultV int) int {
 	if value, exists := os.LookupEnv(env); exists {
 		if intVal, err := strconv.Atoi(value); err != nil {
@@ -96,6 +113,7 @@ func mustGetIntOrDefault(env string, defaultV int) int {
 	return defaultV
 }
 
+// mustHostname answers the hostname of the server answering the request, or panics.
 func mustHostname() string {
 	host, err := os.Hostname()
 	if err != nil {
@@ -104,22 +122,24 @@ func mustHostname() string {
 	return host
 }
 
+// isTemplateRequest answers `true` if the request ought to be handled by a Go template
 func isTemplateRequest(path string) (string, bool) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return "", false
-	}
+	tmplPath := strings.ReplaceAll(path, ".html", ".tmpl")
 
-	if errors.Is(err, os.ErrNotExist) {
-		tmplPath := strings.ReplaceAll(path, ".html", ".tmpl")
-		_, err := os.Stat(tmplPath)
-		return tmplPath, err == nil
+	// if a template exists for the requested file, it's a template request
+	_, err := os.Stat(tmplPath)
+	if err == nil {
+		return tmplPath, true
 	}
 
 	return "", false
 }
 
+// processTemplate executes the template at the supplied path, and returns file path to the output
 func processTemplate(tmplPath string, model interface{}) (string, error) {
+	if isVerbose {
+		log.Printf("Processing %s", tmplPath)
+	}
 
 	var (
 		tmpl    *template.Template
@@ -147,5 +167,16 @@ func processTemplate(tmplPath string, model interface{}) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	if isVerbose {
+		log.Printf("Processed %s to %s", tmplPath, outPath)
+	}
 	return outPath, nil
+}
+
+// stripRoot removes the prefix `root` from `requestPath`
+func stripRoot(root, requestPath string) string {
+	if strings.HasPrefix(requestPath, root) {
+		return requestPath[len(root):]
+	}
+	return requestPath
 }
